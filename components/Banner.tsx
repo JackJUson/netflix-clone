64fd7e2e4555b17b44d77c77f5e036240cfb2794
Jackson <z5397791@ad.unsw.edu.au>
2023-01-01 10:13:10 +1100

    Add movie state variable with useState hook to get random movie at render

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,17 +1,28 @@
-import Image from "next/image"
-import { Movie } from "../typings"
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { Movie } from "../typings";
 
 interface Props {
   netflixOriginals: Movie[];
 }
 
 function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
+
+  console.log(movie);  
+
   return (
     <div>
-      {/* <div>
-        <Image></Image>
-      </div> */}
+      <div>
+        {/* <Image /> */}
+      </div>
     </div>
-  )
+  );
 }
-export default Banner
+export default Banner;
