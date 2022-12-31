b6b7364e4c3d2f2fb619024de1d9e5603309e98d
Jackson <z5397791@ad.unsw.edu.au>
2022-12-31 22:51:50 +1100

    Add tmdb api requests with movies based on rating, trending and genres in request.ts

---
+++ utils/request.ts
@@ -0,0 +1,15 @@
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+const BASE_URL = 'https://api.themoviedb.org/3'
+
+const requests = {
+  fetchTopRated: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchTrending: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_networks=213`,
+  fetchActionMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`,
+  fetchComedyMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`,
+  fetchHorrorMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`,
+  fetchRomanceMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`,
+  fetchDocumentaries: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`,
+}
+
+export default requests
