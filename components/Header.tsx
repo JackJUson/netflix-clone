fdcefe1c1c930d3ae3fec97ba7f22ef4bb8e69a6
Jackson <z5397791@ad.unsw.edu.au>
2022-12-31 20:11:42 +1100

    Add search, bell icons, profile link in second div of header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
 function Header() {
   return (
     <header>
@@ -18,7 +21,18 @@ function Header() {
         </ul>
       </div>
 
-      <div></div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
