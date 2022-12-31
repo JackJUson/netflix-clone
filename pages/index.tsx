05906b08eb95fd78a2e77699258bf28d6412b27b
Jackson <z5397791@ad.unsw.edu.au>
2023-01-01 09:37:21 +1100

    Add async prop function for server side rendering with single promise all in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
+import requests from '../utils/request'
 
-const Home: NextPage = () => {
+const Home = ({ netflixOriginals }) => {
+  console.log(netflixOriginals);
   return (
-    <div className="relative h-[110vh]">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10
+    to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/* Sementic html improves seo performance */}
-      {/* Header */}
       <main>    
-        {/* Banner */}
+        <Banner />
         <section>
           {/* Row */}
           {/* Row */}
@@ -29,4 +29,28 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Home;
+
+export const getServerSideProps = async() => {
+  // Single await for page performance
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+
+}
