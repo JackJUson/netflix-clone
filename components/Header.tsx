bbdf5a8f77d604e05a7a99ef17e1edf62c482f4b
Jackson <z5397791@ad.unsw.edu.au>
2022-12-31 19:31:25 +1100

    Add header component containing navigation tag in components directory

---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <header>
+      <div>
+      <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+        </ul>
+      </div>
+      <div></div>
+    </header>
+  )
+}
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,30 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Building Netflix...
+      <Header />
+      {/* Sementic html improves seo performance */}
+      {/* Header */}
+      <main>    
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+      {/* Modal */}
     </div>
   )
 }
