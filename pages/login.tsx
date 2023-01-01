a7d887aa65d114282ce4c4c2e919543edf7ae2d8
Jackson <z5397791@ad.unsw.edu.au>
2023-01-01 20:18:24 +1100

    Add final styling for sign in and sign up button

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,10 @@
-import Head from "next/head"
-import Image from "next/image"
+import Head from "next/head";
+import Image from "next/image";
 
 function login() {
+
   return (
-    <div className="">
+    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -22,20 +23,31 @@ function login() {
         height={150}
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
-      md:max-w-md md:px-14">
-          <h1>Sign In</h1>
+      <form
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
+      md:max-w-md md:px-14"
+      >
+        <h1 className="text-4xl font-semibold">Sign In</h1>
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type="email" placeholder="Email" className="input"/>
+            <input type="email" placeholder="Email" className="input" />
           </label>
           <label className="inline-block w-full">
-            <input type="password" placeholder="Password" className="input"/>
+            <input type="password" placeholder="Password" className="input" />
           </label>
         </div>
-      </form>
 
+        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+          Sign In
+        </button>
+
+        <div className="text-[grey]">
+          New to Netflix?{' '}
+          <button type="submit" className="text-white hover:underline">Sign up now</button>
+        </div>
+
+      </form>
     </div>
-  )
+  );
 }
-export default login
+export default login;
