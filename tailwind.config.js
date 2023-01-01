9e2481c195cbdcef723fcd12df089c794424c8be
Jackson <z5397791@ad.unsw.edu.au>
2023-01-01 17:42:09 +1100

    Add background gradient on banner image

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-to-b':
+          'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 68%,#141414 100%);',
+      },
+    },
   },
   plugins: [
     require('tailwindcss-textshadow'),
