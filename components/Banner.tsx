6a507246271c8de8e12e81148afc703738850ebb
Jackson <z5397791@ad.unsw.edu.au>
2023-01-01 13:03:49 +1100

    Add banner component style and in globals.css

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { Movie } from "../typings";
 import { baseUrl } from "../constants/baseUrl";
+import { FaPlay } from "react-icons/fa";
+import { InformationCircleIcon } from "@heroicons/react/24/solid";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -19,14 +21,37 @@ function Banner({ netflixOriginals }: Props) {
   console.log(movie);
 
   return (
-    <div>
-      <div className="absolute top-0 left-0 h-[95vh] w-screen">
+    <div
+      className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] 
+    lg:justify-end lg:pb-12"
+    >
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           alt=""
           fill
-          style={{objectFit:"cover"}}
+          style={{ objectFit: "cover" }}
         />
+      </div>
+
+      <h1 className="text-2xl text-shadow-xl font-bold md:text-4xl lg:text-7xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p
+        className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl 
+      lg:text-2xl"
+      >
+        {movie?.overview}
+      </p>
+
+      <div className="flex space-x-3">
+        <button className="bannerButton bg-white text-black">
+          <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" />
+          Play
+        </button>
+        <button className="bannerButton bg-[grey]/70">
+          More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+        </button>
       </div>
     </div>
   );
