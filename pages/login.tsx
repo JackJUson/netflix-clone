266e19bdd34a5b03278b69943a1ca9a021f43f14
Jackson <z5397791@ad.unsw.edu.au>
2023-01-01 20:53:47 +1100

    Add form validation with react hook form in login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,27 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 function login() {
+  const [login, setLogin] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+    <div className="relative flex h-screen w-screen flex-col bg-black 
+    md:items-center md:justify-center md:bg-transparent">
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -24,16 +41,37 @@ function login() {
       />
 
       <form
-        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
-      md:max-w-md md:px-14"
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 
+        py-10 px-6 md:mt-0 md:max-w-md md:px-14"
       >
         <h1 className="text-4xl font-semibold">Sign In</h1>
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type="email" placeholder="Email" className="input" />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[14px] font-light text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
           <label className="inline-block w-full">
-            <input type="password" placeholder="Password" className="input" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <p className="pt-1 pr-1 pl-1 text-[14px] font-light text-orange-500">
+                Your password must contain between 8 to 40 characters.
+              </p>
+            )}
           </label>
         </div>
 
@@ -42,10 +80,11 @@ function login() {
         </button>
 
         <div className="text-[grey]">
-          New to Netflix?{' '}
-          <button type="submit" className="text-white hover:underline">Sign up now</button>
+          New to Netflix?{" "}
+          <button type="submit" className="text-white hover:underline">
+            Sign up now
+          </button>
         </div>
-
       </form>
     </div>
   );
