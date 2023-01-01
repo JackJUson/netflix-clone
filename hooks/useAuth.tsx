eea5afa3008e262eea4ae9098b76ab673041523a
Jackson <z5397791@ad.unsw.edu.au>
2023-01-01 21:56:04 +1100

    Add useRouter to change into login page afer user sign up

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,23 +1,31 @@
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
   User,
 } from 'firebase/auth'
 
-import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
 import { auth } from '../firebase'
 
 function useAuth() {
     const [loading, setLoading] = useState(false);
     const [user, setUser] = useState<User | null>(null);
+    const router = useRouter();
 
     const signUp = async(email: string, password: string) => {
       setLoading(true);
 
       await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
         setUser(userCredential.user)
+        router.push('/');
+        setLoading(false);
       })
     }
-    
+
+
   return 
 }
 export default useAuth;
