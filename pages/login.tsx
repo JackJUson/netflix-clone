db1e5f4bd4064db69ff641123cdfb098ef3cd1ed
Jackson <z5397791@ad.unsw.edu.au>
2023-01-01 18:54:57 +1100

    Add background image and logo img to login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,27 @@
+import Head from "next/head"
+import Image from "next/image"
+
 function login() {
   return (
-    <div>login</div>
+    <div className="">
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+        src="https://rb.gy/p2hphi"
+        alt=""
+        fill
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        style={{ objectFit: "cover" }}
+      />
+      <img
+        src="https://rb.gy/ulxxee"
+        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+        width={150}
+        height={150}
+      />
+    </div>
   )
 }
 export default login
