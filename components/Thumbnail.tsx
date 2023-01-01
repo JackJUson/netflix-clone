6dec8b8165067aab0582c450aafc6f07a3fbe821
Jackson <z5397791@ad.unsw.edu.au>
2023-01-01 17:41:28 +1100

    Adjust thumbnail dimension on screen size

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -2,19 +2,21 @@ import Image from "next/image";
 import { Movie } from "../typings";
 
 interface Props {
+  // movie: Movie | DocumentData;
   movie: Movie;
 }
 
 function Thumbnail({ movie }: Props) {
   return (
     <div className="relative h-28 min-w-[180px] cursor-pointer transition 
-    duration-300 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+    duration-300 ease-out sm:hover:scale-[103%] md:h-36 md:min-w-[260px]
+    lg:h-44 lg:min-w-[340px]">
       <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
         }`}
         alt=""
-        className="rounded-sm md:rounded object-cover"
+        className="md:rounded object-cover rounded-md"
         fill
       />
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -28,8 +28,7 @@ const Home = ({
 }: Props) => {
   return (
     <div
-      className="relative h-screen bg-gradient-to-b from-gray-900/10
-    to-[#010511] lg:h-[140vh]"
+      className="relative h-screen bg-gradient-to-b lg:h-[140vh]"
     >
       <Head>
         <title>Home - Netflix</title>
