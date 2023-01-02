0217c4532e75d90cb97382f2612ecc7930eae2d7
Jackson <z5397791@ad.unsw.edu.au>
2023-01-02 22:59:40 +1100

    Add show movie and modal react hook on thumbnail image click

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Movie } from "../typings";
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
 
 interface Props {
   // movie: Movie | DocumentData;
@@ -7,10 +9,17 @@ interface Props {
 }
 
 function Thumbnail({ movie }: Props) {
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+
   return (
     <div className="relative h-28 min-w-[180px] cursor-pointer transition 
     duration-300 ease-out sm:hover:scale-[103%] md:h-36 md:min-w-[260px]
-    lg:h-52 lg:min-w-[380px]">
+    lg:h-52 lg:min-w-[380px]"
+    onClick={() => {
+      setCurrentMovie(movie);
+      setShowModal(true);
+    }}>
       <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
