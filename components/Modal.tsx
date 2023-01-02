b7bfc89c6db69b8bb95820525e69d37e071fae06
Jackson <z5397791@ad.unsw.edu.au>
2023-01-02 23:02:09 +1100

    Written implementation for useEffect to fetch movie for click on specific modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,13 +1,47 @@
 import MuiModal from "@mui/material/Modal";
-import { modalState } from "../atoms/modalAtom";
-import { useRecoilValue, useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
+import { useRecoilState } from "recoil";
 import { XMarkIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
+import { Element, Genre } from "../typings";
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState);
+  const [movie, setMovie] = useRecoilState(movieState);
+  const [trailer, setTrailer] = useState("");
+  const [genres, setGenres] = useState<Genre[]>([]);
+
   const handleClose = () => {
     setShowModal(false);
   };
+
+  useEffect(() => {
+    if (!movie) return;
+
+    async function fetchMovie() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+          movie?.media_type === "tv" ? "tv" : "movie"
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=en-US&append_to_response=videos`
+      ).then((response) => response.json());
+
+      if (data?.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === "Trailer"
+        );
+        setTrailer(data.videos?.results[index]?.key)
+      }
+      if (data?.genres) {
+        setGenres(data.genres)
+      }
+    }
+
+    fetchMovie();
+  }, [movie]);
+
+  console.log(trailer);
 
   return (
     <MuiModal open={showModal} onClose={handleClose}>
@@ -19,6 +53,8 @@ function Modal() {
         >
           <XMarkIcon className="h-7 w-7" />
         </button>
+
+        <div></div>
       </>
     </MuiModal>
   );
