851a2990d777ff4499d896be7ef927dd25a05f2d
Jackson <z5397791@ad.unsw.edu.au>
2023-01-03 20:50:12 +1100

    Add plan page with sign out button using useAuth custom function

---
+++ components/Plan.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Link from "next/link";
+import useAuth from "../hooks/useAuth";
+
+function Plan() {
+  const { logout } = useAuth();
+
+  return (
+    <div>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className="border-b border-white/10 bg-[#141414]">
+        <Link href="/">
+          <img
+            src="https://rb.gy/ulxxee"
+            alt="Netflix"
+            width={150}
+            height={90}
+            className="cursor-pointer object-contain"
+          />
+        </Link>
+        <button
+          className="text-lg font-medium hover:underline"
+          onClick={logout}
+        >
+          Sign Out
+        </button>
+      </header>
+    </div>
+  );
+}
+export default Plan;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { modalState } from "../atoms/modalAtom";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
+import Plan from "../components/Plan";
 import Row from "../components/Row";
 import useAuth from "../hooks/useAuth";
 import { Movie } from "../typings";
@@ -32,8 +33,11 @@ const Home = ({
 }: Props) => {
   const { loading, logout } = useAuth();
   const showModal = useRecoilValue(modalState);
+  const subscription = false;
 
-  if (loading) return null;
+  if (loading || subscription === null) return null;
+
+  if (!subscription) return <Plan />
 
   return (
     <div
