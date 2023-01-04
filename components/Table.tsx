44e36ac637aaff071d67676f88d4c3a6daacb8bd
Jackson <z5397791@ad.unsw.edu.au>
2023-01-04 22:36:44 +1100

    Add table row and title style with divide y row for table body

--- components/Table.tsx
+++ components/Table.tsx
@@ -7,15 +7,17 @@ interface Props {
 function Table({ products }: Props) {
   return (
     <table>
-      <tbody>
-        <tr>
-          <td>Monthly price</td>
-          {products.map(product => (
-            <td key={product.id}>A${product.prices[0].unit_amount! / 100}</td>
+      <tbody className="divide-y divide-[gray]">
+        <tr className="tableRow">
+          <td className="tableDataTitle">Monthly price</td>
+          {products.map((product) => (
+            <td key={product.id} className="tableDataPrice">
+              A${product.prices[0].unit_amount! / 100}
+            </td>
           ))}
         </tr>
       </tbody>
     </table>
-  )
+  );
 }
-export default Table
+export default Table;
