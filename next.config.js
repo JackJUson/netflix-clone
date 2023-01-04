e02803a5c82ceaba6c69e4b6d7ca23313e71ece8
Jackson <z5397791@ad.unsw.edu.au>
2023-01-04 22:05:16 +1100

    Add transpile packages for stripe firestore payments in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  transpilePackages: ['@stripe/firestore-stripe-payments'],
   reactStrictMode: true,
   images: {
     domains: ['image.tmdb.org', "rb.gy"],
