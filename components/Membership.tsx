c9cef964f50ca187b7464b0cdc1af87a8bccc7b1
Jackson <z5397791@ad.unsw.edu.au>
2023-01-05 12:04:58 +1100

    Add manage subscription function on click by cancel membership button

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -8,14 +8,24 @@ function Membership() {
   const subscription = useSubscription(user);
   const [billingLoading, setBillingLoading] = useState(false);
 
+  const manageSubscription = () => {
+    if (subscription) {
+      setBillingLoading(true);
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <h4>Membership & Billing</h4>
+    <div
+      className="mt-6 grid grid-cols-1 gap-x-4 border px-4 md:grid-cols-4 
+    md:border-x-0 md:border-t md:border-b-0 md:px-0"
+    >
+      <div className="space-y-2 py-4">
+        <h4 className="text-lg text-[gray]">Membership & Billing</h4>
         <button
           disabled={billingLoading || !subscription}
           className="h-10 w-3/5 whitespace-nowrap bg-gray-300 py-2 text-sm 
           font-medium text-black shadow-md hover:bg-gray-200 md:w-4/5"
+          onClick={manageSubscription}
         >
           {billingLoading ? (
             <Loader color="dark:fill-[#e50914]" />
