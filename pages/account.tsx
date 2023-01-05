8fa9867b8086a24601099119cf877a268219b3d2
Jackson <z5397791@ad.unsw.edu.au>
2023-01-05 11:58:30 +1100

    Add settings div with signout button to logout user

--- pages/account.tsx
+++ pages/account.tsx
@@ -12,7 +12,7 @@ interface Props {
 }
 
 function Account({ products }: Props) {
-  const { user } = useAuth();
+  const { user, logout, loading } = useAuth();
   const subscription = useSubscription(user);
   return (
     <div>
@@ -66,6 +66,19 @@ function Account({ products }: Props) {
           </div>
           <p className="cursor-pointer text-blue-500 hover:underline md:text-right">
             Change plan
+          </p>
+        </div>
+
+        <div
+          className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 
+        md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0"
+        >
+          <h4 className="text-lg text-[gray]">Settings</h4>
+          <p
+            className="col-span-3 cursor-pointer text-blue-500 hover:underline"
+            onClick={logout}
+          >
+            Sign out of all devices
           </p>
         </div>
       </main>
