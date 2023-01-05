f2f8e227fd6b123110d4d87d9f20dda1c632021b
Jackson <z5397791@ad.unsw.edu.au>
2023-01-05 16:40:06 +1100

    Add document data to movies prop in row component

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,11 +1,12 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import { DocumentData } from "firebase/firestore";
 import { useRef, useState } from "react";
 import { Movie } from "../typings";
 import Thumbnail from "./Thumbnail";
 
 interface Props {
   title: string;
-  movies: Movie[];
+  movies: Movie[] | DocumentData[];
 }
 
 function Row({ title, movies }: Props) {
@@ -27,7 +28,7 @@ function Row({ title, movies }: Props) {
   };
 
   return (
-    <div className="space-y-0.5 sm:space-y-2 sm:h-40 lg:h-60">
+    <div className="space-y-0.5 sm:h-40 sm:space-y-2 lg:h-60">
       <h2
         className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5]
       transition duration-300 hover:text-white md:text-2xl lg:text-3xl"
@@ -44,13 +45,13 @@ function Row({ title, movies }: Props) {
 
         <div
           ref={rowRef}
-          className="flex items-center space-x-1 sm:space-x-3 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
+          className="flex items-center space-x-1 overflow-x-scroll scrollbar-hide sm:space-x-3 md:space-x-2.5 md:p-2"
         >
           {movies.map((movie) => (
             <Thumbnail movie={movie} key={movie.id} />
           ))}
         </div>
-            
+
         <ChevronRightIcon
           className={`absolute top-0 bottom-0 right-2 z-40 m-auto 
           h-9 w-9 cursor-pointer opacity-0 transition group-hover:opacity-100 hover:scale-125 
