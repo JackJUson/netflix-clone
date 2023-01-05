d8b7e5dc5e5de91eab4eb51c6d05dd3416781e1d
Jackson <z5397791@ad.unsw.edu.au>
2023-01-05 15:32:26 +1100

    Written implementation for firebase shapsnot to retrive database and user uid in useEffect

--- hooks/useList.tsx
+++ hooks/useList.tsx
@@ -1,12 +1,27 @@
-import { DocumentData } from 'firebase/firestore'
-import { useEffect, useState } from 'react'
-import { Movie } from '../typings'
+import { collection, DocumentData, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Movie } from "../typings";
+import { db } from "../firebase";
 
-function useList() {
+function useList(uid: string | undefined) {
   const [list, setList] = useState<Movie[] | DocumentData>([]);
 
-  return (
-    <div>useList</div>
-  )
+  useEffect(() => {
+    if (!uid) return;
+
+    return onSnapshot(
+      collection(db, "customers", uid, "myList"),
+      (snapshot) => {
+        setList(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+        );
+      }
+    );
+  }, [db, uid]);
+
+  return list;
 }
-export default useList
+export default useList;
