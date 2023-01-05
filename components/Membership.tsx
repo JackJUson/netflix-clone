5a52a9d4ff46f4172c5ec2feabf2e43b5a3f7321
Jackson <z5397791@ad.unsw.edu.au>
2023-01-05 12:02:42 +1100

    Add cancel membership button with bill loading hook

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -1,6 +1,30 @@
+import useAuth from "../hooks/useAuth";
+import useSubscription from "../hooks/useSubscription";
+import { useState } from "react";
+import Loader from "./Loader";
+
 function Membership() {
+  const { user } = useAuth();
+  const subscription = useSubscription(user);
+  const [billingLoading, setBillingLoading] = useState(false);
+
   return (
-    <div>Membership</div>
-  )
+    <div>
+      <div>
+        <h4>Membership & Billing</h4>
+        <button
+          disabled={billingLoading || !subscription}
+          className="h-10 w-3/5 whitespace-nowrap bg-gray-300 py-2 text-sm 
+          font-medium text-black shadow-md hover:bg-gray-200 md:w-4/5"
+        >
+          {billingLoading ? (
+            <Loader color="dark:fill-[#e50914]" />
+          ) : (
+            "Cancel Membership"
+          )}
+        </button>
+      </div>
+    </div>
+  );
 }
-export default Membership
+export default Membership;
